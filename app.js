882e3af3fd700cf07735475a5cfc557307eed1be
A.Vihrogonov <vihrogonov@protonmail.com>
2020-02-11 16:07:38 +0000

    create tab 'about' & 'contact' functionality

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ addForm.addEventListener('submit', function(e){
   li.appendChild(bookName);
   li.appendChild(deleteBtn);
   list.appendChild(li);
-  
 });
 
 // hide books
@@ -42,4 +41,24 @@ hideBox.addEventListener('change', function(e){
     list.style.display = "initial";
   }
 });
+
+// filter books
+const searchBox = document.forms['search-books'].querySelector('input');
+// keyup event
+searchBox.addEventListener('keyup', function(e){
+  const term = e.target.value.toLowerCase();
+  const books = list.getElementsByTagName('li');
+
+  Array.from(books).forEach(function(book){
+    const title = book.firstElementChild.textContent;
+
+    if(title.toLocaleLowerCase().indexOf(term) != -1){
+      book.style.display = 'block';
+    } else {
+      book.style.display = 'none';
+    }
+  });
+});
+
+
 
