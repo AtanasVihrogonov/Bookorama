8250bc03b00de2723e672adb28acd8b7dddea5ba
A.Vihrogonov <vihrogonov@protonmail.com>
2020-02-11 15:34:42 +0000

    hide books functionality

--- app.js
+++ app.js
@@ -7,11 +7,39 @@ list.addEventListener('click', function(e){
   }
 });
 
-// add book-list
+// add books
 const addForm = document.forms['add-book'];
-
 addForm.addEventListener('submit', function(e){
   e.preventDefault();
   const value = addForm.querySelector('input[type="text"]').value;
-  console.log(value);
+  
+  // create elements
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  // add content
+  deleteBtn.textContent = 'delete';
+  bookName.textContent = value;
+
+  // add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
+  // append to document
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
+  
 });
+
+// hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+  if(hideBox.checked){
+    list.style.display = "none";
+  } else {
+    list.style.display = "initial";
+  }
+});
+
